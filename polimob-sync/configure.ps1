308b51dd2921f1aea19acc2ec84910ed2d70fcfc
krlitozzolivares <krlitozz.olivares@gmail.com>
2026-09-25 01:40:14 -0500

    chore: bump configurator to v0.6.2

--- polimob-sync/configure.ps1
+++ polimob-sync/configure.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference="Stop"
-$Version="0.6.1"
+$Version="0.6.2"
 $Mozaik="C:\Mozaik"
 $AppDir=Join-Path $env:ProgramData "OCRE\PolimobSync"
 $Profile=Join-Path $AppDir "profile.json"
